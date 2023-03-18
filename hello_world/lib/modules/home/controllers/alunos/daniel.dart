e86ac5c2c41e364f1821748c388fec4d3ef23eda
daniel.manoel <daniel.manoel@djsystem.com>
2023-03-18 11:21:38 -0300

    função criada no controller

---
+++ hello_world/lib/modules/home/controllers/alunos/daniel.dart
@@ -0,0 +1,6 @@
+class DanielActions {
+  soma(int n) {
+    int soma = n + n;
+    return print("Soma de $n + $n = $soma");
+  }
+}
--- hello_world/lib/modules/home/controllers/home.controller.dart
+++ hello_world/lib/modules/home/controllers/home.controller.dart
@@ -1,11 +1,16 @@
+import 'package:hello_world/modules/home/controllers/alunos/daniel.dart';
 import 'package:hello_world/modules/home/controllers/alunos/deivide.dart';
 
 class HomeController {
   DeivideActions deivideActions = DeivideActions();
-
+  DanielActions danielActions = DanielActions();
   //funcoes dos alunos
   deivide() {
     deivideActions.eat();
     deivideActions.heRun();
+  }
+
+  daniel() {
+    danielActions.soma(50);
   }
 }
