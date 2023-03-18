c4e311ff07793a00ef8657e7b5a4dc8852dc4a22
MarioGilson <mariogilsonon.ti@gmail.com>
2023-03-18 11:23:27 -0300

    Novas funções

--- hello_world/lib/modules/home/controllers/home.controller.dart
+++ hello_world/lib/modules/home/controllers/home.controller.dart
@@ -1,11 +1,15 @@
 import 'package:hello_world/modules/home/controllers/alunos/deivide.dart';
-
+import 'package:hello_world/modules/home/controllers/alunos/mario.kart.dart';
 class HomeController {
   DeivideActions deivideActions = DeivideActions();
-
+  MKartActions marioKartActions = MKartActions();
   //funcoes dos alunos
   deivide() {
     deivideActions.eat();
     deivideActions.heRun();
+  }
+  mario(){
+    marioKartActions.queMario();
+    marioKartActions.matarCogumelo();
   }
 }
