2388c040791a3d1e2d285b8de3ea021e9df4c226
Isabelly Lima <bellyfernanda15@gmail.com>
2023-03-18 11:04:13 -0300

    commit isa

--- hello_world/lib/modules/home/controllers/home.controller.dart
+++ hello_world/lib/modules/home/controllers/home.controller.dart
@@ -1,11 +1,18 @@
 import 'package:hello_world/modules/home/controllers/alunos/deivide.dart';
 
+import 'alunos/isa.dart';
+
 class HomeController {
   DeivideActions deivideActions = DeivideActions();
+  Estudos estudos = Estudos();
 
   //funcoes dos alunos
   deivide() {
     deivideActions.eat();
     deivideActions.heRun();
+  }
+
+  isa(){
+    estudos.estudar();
   }
 }
