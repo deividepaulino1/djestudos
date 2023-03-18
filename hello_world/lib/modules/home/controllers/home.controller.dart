a4778ef25a06ec6bdc402fa39661d40aa9b023ce
deividepaulino1 <deividepcsvlog@gmail.com>
2023-03-18 11:28:35 -0300

    checkout jorge

--- hello_world/lib/modules/home/controllers/home.controller.dart
+++ hello_world/lib/modules/home/controllers/home.controller.dart
@@ -1,7 +1,16 @@
 import 'package:hello_world/modules/home/controllers/alunos/deivide.dart';
 
+import 'alunos/jorge.dart';
+
 class HomeController {
   JorgeActions jorgeActions = JorgeActions();
+
+  DeivideActions deivideActions = DeivideActions();
+
+  deivide() {
+    deivideActions.eat();
+    deivideActions.heRun();
+  }
 
   //funcoes dos alunos
   jorge() {
